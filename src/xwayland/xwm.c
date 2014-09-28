0eb12a98a740277d7eff40af5fdb795182b2dc51
Jari Vetoniemi <mailroxas@gmail.com>
2014-09-29 01:30:04 +0300

    Take out xcb hard deps.

--- src/xwayland/xwm.c
+++ src/xwayland/xwm.c
@@ -1,5 +1,3 @@
-/* This is mostly based on swc's xwm.c */
-
 #include "xwm.h"
 #include "compositor/compositor.h"
 #include "compositor/surface.h"
@@ -8,6 +6,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
+#include <dlfcn.h>
 
 #include <xcb/composite.h>
 #include <xcb/xcb_ewmh.h>
@@ -15,6 +14,13 @@
 
 #include <wayland-server.h>
 #include <wayland-util.h>
+
+struct wlc_x11_window {
+   struct wlc_surface *surface;
+   struct wl_list link;
+   xcb_window_t id;
+   bool override_redirect;
+};
 
 enum atom_name {
    WL_SURFACE_ID,
@@ -24,22 +30,161 @@ enum atom_name {
    ATOM_LAST
 };
 
-struct wlc_x11_window {
-   struct wlc_surface *surface;
-   struct wl_list link;
-   xcb_window_t id;
-   bool override_redirect;
-};
-
 static struct {
    struct wl_event_source *event_source;
+   struct wl_list windows, unpaired_windows;
+} xwm;
+
+static struct {
    xcb_screen_t *screen;
    xcb_connection_t *connection;
    xcb_atom_t atoms[ATOM_LAST];
    xcb_ewmh_connection_t ewmh;
    xcb_window_t window;
-   struct wl_list windows, unpaired_windows;
-} xwm;
+
+   struct {
+      void *xcb_handle;
+
+      xcb_connection_t* (*xcb_connect_to_fd)(int, xcb_auth_info_t*);
+      void (*xcb_disconnect)(xcb_connection_t*);
+      void (*xcb_prefetch_extension_data)(xcb_connection_t*, xcb_extension_t*);
+      int (*xcb_flush)(xcb_connection_t*);
+      int (*xcb_connection_has_error)(xcb_connection_t*);
+      const xcb_setup_t* (*xcb_get_setup)(xcb_connection_t*);
+      xcb_screen_iterator_t (*xcb_setup_roots_iterator)(const xcb_setup_t*);
+      uint32_t (*xcb_generate_id)(xcb_connection_t*);
+      xcb_void_cookie_t (*xcb_create_window_checked)(xcb_connection_t*, uint8_t, xcb_window_t, xcb_window_t, int16_t, int16_t, uint16_t, uint16_t, uint16_t, uint16_t, xcb_visualid_t, uint32_t, const uint32_t*);
+      xcb_void_cookie_t (*xcb_destroy_window)(xcb_connection_t*, xcb_window_t);
+      xcb_void_cookie_t (*xcb_map_window_checked)(xcb_connection_t*, xcb_window_t);
+      xcb_void_cookie_t (*xcb_change_property)(xcb_connection_t*, uint8_t, xcb_window_t, xcb_atom_t, xcb_atom_t, uint8_t, uint32_t, const void*);
+      xcb_void_cookie_t (*xcb_change_window_attributes_checked)(xcb_connection_t*, xcb_window_t, uint32_t, const uint32_t*);
+      xcb_void_cookie_t (*xcb_configure_window_checked)(xcb_connection_t*, xcb_window_t, uint16_t, const uint32_t*);
+      xcb_void_cookie_t (*xcb_set_selection_owner_checked)(xcb_connection_t*, xcb_window_t, xcb_atom_t, xcb_timestamp_t);
+      xcb_intern_atom_cookie_t (*xcb_intern_atom)(xcb_connection_t*, uint8_t, uint16_t, const char*);
+      xcb_intern_atom_reply_t* (*xcb_intern_atom_reply)(xcb_connection_t*, xcb_intern_atom_cookie_t, xcb_generic_error_t**);
+      xcb_generic_error_t* (*xcb_request_check)(xcb_connection_t*, xcb_void_cookie_t);
+      xcb_generic_event_t* (*xcb_poll_for_event)(xcb_connection_t*);
+      xcb_query_extension_reply_t* (*xcb_get_extension_data)(xcb_connection_t*, xcb_extension_t*);
+
+      xcb_intern_atom_cookie_t* (*xcb_ewmh_init_atoms)(xcb_connection_t*, xcb_ewmh_connection_t*);
+      uint8_t (*xcb_ewmh_init_atoms_replies)(xcb_ewmh_connection_t*, xcb_intern_atom_cookie_t*, xcb_generic_error_t**);
+
+      xcb_void_cookie_t (*xcb_composite_redirect_subwindows_checked)(xcb_connection_t*, xcb_window_t, uint8_t);
+      xcb_extension_t *xcb_composite_id;
+   } api;
+} x11;
+
+static bool
+xcb_load(void)
+{
+   const char *lib = "libxcb.so", *func = NULL;
+
+   if (!(x11.api.xcb_handle = dlopen(lib, RTLD_LAZY))) {
+      fprintf(stderr, "-!- %s\n", dlerror());
+      return false;
+   }
+
+#define load(x) (x11.api.x = dlsym(x11.api.xcb_handle, (func = #x)))
+
+   if (!load(xcb_connect_to_fd))
+      goto function_pointer_exception;
+   if (!load(xcb_prefetch_extension_data))
+      goto function_pointer_exception;
+   if (!load(xcb_flush))
+      goto function_pointer_exception;
+   if (!load(xcb_connection_has_error))
+      goto function_pointer_exception;
+   if (!load(xcb_get_setup))
+      goto function_pointer_exception;
+   if (!load(xcb_setup_roots_iterator))
+      goto function_pointer_exception;
+   if (!load(xcb_generate_id))
+      goto function_pointer_exception;
+   if (!load(xcb_create_window_checked))
+      goto function_pointer_exception;
+   if (!load(xcb_destroy_window))
+      goto function_pointer_exception;
+   if (!load(xcb_map_window_checked))
+      goto function_pointer_exception;
+   if (!load(xcb_change_property))
+      goto function_pointer_exception;
+   if (!load(xcb_change_window_attributes_checked))
+      goto function_pointer_exception;
+   if (!load(xcb_configure_window_checked))
+      goto function_pointer_exception;
+   if (!load(xcb_set_selection_owner_checked))
+      goto function_pointer_exception;
+   if (!load(xcb_intern_atom))
+      goto function_pointer_exception;
+   if (!load(xcb_intern_atom_reply))
+      goto function_pointer_exception;
+   if (!load(xcb_request_check))
+      goto function_pointer_exception;
+   if (!load(xcb_poll_for_event))
+      goto function_pointer_exception;
+   if (!load(xcb_get_extension_data))
+      goto function_pointer_exception;
+
+#undef load
+
+   return true;
+
+function_pointer_exception:
+   fprintf(stderr, "-!- Could not load function '%s' from '%s'\n", func, lib);
+   return false;
+}
+
+static bool
+xcb_ewmh_load(void)
+{
+   const char *lib = "libxcb-ewmh.so", *func = NULL;
+
+   if (!(x11.api.xcb_handle = dlopen(lib, RTLD_LAZY))) {
+      fprintf(stderr, "-!- %s\n", dlerror());
+      return false;
+   }
+
+#define load(x) (x11.api.x = dlsym(x11.api.xcb_handle, (func = #x)))
+
+   if (!load(xcb_ewmh_init_atoms))
+      goto function_pointer_exception;
+   if (!load(xcb_ewmh_init_atoms_replies))
+      goto function_pointer_exception;
+
+#undef load
+
+   return true;
+
+function_pointer_exception:
+   fprintf(stderr, "-!- Could not load function '%s' from '%s'\n", func, lib);
+   return false;
+}
+
+static bool
+xcb_composite_load(void)
+{
+   const char *lib = "libxcb-composite.so", *func = NULL;
+
+   if (!(x11.api.xcb_handle = dlopen(lib, RTLD_LAZY))) {
+      fprintf(stderr, "-!- %s\n", dlerror());
+      return false;
+   }
+
+#define load(x) (x11.api.x = dlsym(x11.api.xcb_handle, (func = #x)))
+
+   if (!load(xcb_composite_redirect_subwindows_checked))
+      goto function_pointer_exception;
+   if (!load(xcb_composite_id))
+      goto function_pointer_exception;
+
+#undef load
+
+   return true;
+
+function_pointer_exception:
+   fprintf(stderr, "-!- Could not load function '%s' from '%s'\n", func, lib);
+   return false;
+}
 
 /**
  * TODO: change to hashmap, instead of wl_list
@@ -81,8 +226,8 @@ wlc_x11_window_resize(struct wlc_x11_window *win, const uint32_t width, const ui
 {
    static const uint32_t mask = XCB_CONFIG_WINDOW_WIDTH | XCB_CONFIG_WINDOW_HEIGHT;
    const uint32_t values[] = { width, height };
-   xcb_configure_window(xwm.connection, win->id, mask, (uint32_t*)&values);
-   xcb_flush(xwm.connection);
+   x11.api.xcb_configure_window_checked(x11.connection, win->id, mask, (uint32_t*)&values);
+   x11.api.xcb_flush(x11.connection);
 }
 
 static void
@@ -103,7 +248,7 @@ link_surface(struct wlc_compositor *compositor, struct wlc_x11_window *win, cons
    }
 
    wlc_x11_window_resize(win, view->geometry.w, view->geometry.h);
-   xcb_flush(xwm.connection);
+   x11.api.xcb_flush(x11.connection);
 
    wl_list_insert(&xwm.windows, &win->link);
 }
@@ -116,7 +261,7 @@ x11_event(int fd, uint32_t mask, void *data)
 
    int count = 0;
    xcb_generic_event_t *event;
-   while ((event = xcb_poll_for_event(xwm.connection))) {
+   while ((event = x11.api.xcb_poll_for_event(x11.connection))) {
       switch (event->response_type & ~0x80) {
          case XCB_CREATE_NOTIFY: {
             xcb_create_notify_event_t *ev = (xcb_create_notify_event_t*)event;
@@ -132,7 +277,7 @@ x11_event(int fd, uint32_t mask, void *data)
          break;
          case XCB_MAP_REQUEST: {
             xcb_map_request_event_t *ev = (xcb_map_request_event_t*)event;
-            xcb_map_window(xwm.connection, ev->window);
+            x11.api.xcb_map_window_checked(x11.connection, ev->window);
          }
          break;
          case XCB_CONFIGURE_REQUEST:
@@ -141,7 +286,7 @@ x11_event(int fd, uint32_t mask, void *data)
          break;
          case XCB_CLIENT_MESSAGE: {
             xcb_client_message_event_t *ev = (xcb_client_message_event_t*)event;
-            if (ev->type == xwm.atoms[WL_SURFACE_ID]) {
+            if (ev->type == x11.atoms[WL_SURFACE_ID]) {
                struct wlc_x11_window *win;
                if ((win = wlc_x11_window_for_id(&xwm.unpaired_windows, ev->window)))
                   link_surface(compositor, win, ev->data.data32[0]);
@@ -154,21 +299,24 @@ x11_event(int fd, uint32_t mask, void *data)
       count += 1;
    }
 
-   xcb_flush(xwm.connection);
+   x11.api.xcb_flush(x11.connection);
    return count;
 }
 
 bool
 wlc_xwm_init(struct wlc_compositor *compositor, const int fd)
 {
-   xwm.connection = xcb_connect_to_fd(fd, NULL);
-   if (xcb_connection_has_error(xwm.connection))
+   if (!xcb_load() || !xcb_ewmh_load() || !xcb_composite_load())
+      goto fail;
+
+   x11.connection = x11.api.xcb_connect_to_fd(fd, NULL);
+   if (x11.api.xcb_connection_has_error(x11.connection))
       goto xcb_connection_fail;
 
-   xcb_prefetch_extension_data(xwm.connection, &xcb_composite_id);
+   x11.api.xcb_prefetch_extension_data(x11.connection, x11.api.xcb_composite_id);
 
    xcb_intern_atom_cookie_t *ewmh_cookies;
-   if (!(ewmh_cookies = xcb_ewmh_init_atoms(xwm.connection, &xwm.ewmh)))
+   if (!(ewmh_cookies = x11.api.xcb_ewmh_init_atoms(x11.connection, &x11.ewmh)))
       goto emwh_init_atoms_fail;
 
    struct {
@@ -183,16 +331,16 @@ wlc_xwm_init(struct wlc_compositor *compositor, const int fd)
 
    xcb_intern_atom_cookie_t atom_cookies[ATOM_LAST];
    for (int i = 0; i < ATOM_LAST; ++i)
-      atom_cookies[map[i].atom] = xcb_intern_atom(xwm.connection, 0, strlen(map[i].name), map[i].name);
+      atom_cookies[map[i].atom] = x11.api.xcb_intern_atom(x11.connection, 0, strlen(map[i].name), map[i].name);
 
-   const xcb_setup_t *setup = xcb_get_setup(xwm.connection);
-   xcb_screen_iterator_t screen_iterator = xcb_setup_roots_iterator(setup);
-   xwm.screen = screen_iterator.data;
+   const xcb_setup_t *setup = x11.api.xcb_get_setup(x11.connection);
+   xcb_screen_iterator_t screen_iterator = x11.api.xcb_setup_roots_iterator(setup);
+   x11.screen = screen_iterator.data;
 
    /* Try to select for substructure redirect. */
    unsigned int mask = XCB_CW_EVENT_MASK;
    unsigned int value = XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY | XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT;
-   xcb_void_cookie_t change_attributes_cookie = xcb_change_window_attributes(xwm.connection, xwm.screen->root, mask, &value);
+   xcb_void_cookie_t change_attributes_cookie = x11.api.xcb_change_window_attributes_checked(x11.connection, x11.screen->root, mask, &value);
 
    if (!(xwm.event_source = wl_event_loop_add_fd(compositor->event_loop, fd, WL_EVENT_READABLE, &x11_event, compositor)))
       goto event_source_fail;
@@ -201,35 +349,35 @@ wlc_xwm_init(struct wlc_compositor *compositor, const int fd)
    wl_list_init(&xwm.unpaired_windows);
 
    const xcb_query_extension_reply_t *composite_extension;
-   if (!(composite_extension = xcb_get_extension_data(xwm.connection, &xcb_composite_id)) || !composite_extension->present)
+   if (!(composite_extension = x11.api.xcb_get_extension_data(x11.connection, x11.api.xcb_composite_id)) || !composite_extension->present)
       goto composite_extension_fail;
 
-   xcb_void_cookie_t redirect_subwindows_cookie = xcb_composite_redirect_subwindows_checked(xwm.connection, xwm.screen->root, XCB_COMPOSITE_REDIRECT_MANUAL);
+   xcb_void_cookie_t redirect_subwindows_cookie = x11.api.xcb_composite_redirect_subwindows_checked(x11.connection, x11.screen->root, XCB_COMPOSITE_REDIRECT_MANUAL);
 
    xcb_generic_error_t *error;
-   if ((error = xcb_request_check(xwm.connection, change_attributes_cookie)))
+   if ((error = x11.api.xcb_request_check(x11.connection, change_attributes_cookie)))
       goto change_attributes_fail;
 
-   if ((error = xcb_request_check(xwm.connection, redirect_subwindows_cookie)))
+   if ((error = x11.api.xcb_request_check(x11.connection, redirect_subwindows_cookie)))
       goto redirect_subwindows_fail;
 
-   if (!(xwm.window = xcb_generate_id(xwm.connection)))
+   if (!(x11.window = x11.api.xcb_generate_id(x11.connection)))
       goto window_fail;
 
-   xcb_create_window(xwm.connection, 0, xwm.window, xwm.screen->root,
+   x11.api.xcb_create_window_checked(x11.connection, 0, x11.window, x11.screen->root,
          0, 0, 1, 1, 0, XCB_WINDOW_CLASS_INPUT_ONLY,
          XCB_COPY_FROM_PARENT, 0, NULL);
 
-   xcb_ewmh_init_atoms_replies(&xwm.ewmh, ewmh_cookies, &error);
+   x11.api.xcb_ewmh_init_atoms_replies(&x11.ewmh, ewmh_cookies, &error);
 
    if (error)
       goto emwh_init_atom_replies_fail;
 
    for (int i = 0; i < ATOM_LAST; ++i) {
-      xcb_intern_atom_reply_t *atom_reply = xcb_intern_atom_reply(xwm.connection, atom_cookies[map[i].atom], &error);
+      xcb_intern_atom_reply_t *atom_reply = x11.api.xcb_intern_atom_reply(x11.connection, atom_cookies[map[i].atom], &error);
 
       if (atom_reply && !error)
-         xwm.atoms[map[i].atom] = atom_reply->atom;
+         x11.atoms[map[i].atom] = atom_reply->atom;
 
       if (atom_reply)
          free(atom_reply);
@@ -238,13 +386,13 @@ wlc_xwm_init(struct wlc_compositor *compositor, const int fd)
          goto atom_get_fail;
    }
 
-   xcb_set_selection_owner(xwm.connection, xwm.window, xwm.atoms[WM_S0], XCB_CURRENT_TIME);
-   xcb_flush(xwm.connection);
+   x11.api.xcb_set_selection_owner_checked(x11.connection, x11.window, x11.atoms[WM_S0], XCB_CURRENT_TIME);
+   x11.api.xcb_flush(x11.connection);
 
    fprintf(stdout, "-!- xwm started\n");
    return true;
 
-   // TODO: error handling
+   // TODO: error handling or prints whatever
 xcb_connection_fail:
 emwh_init_atoms_fail:
 event_source_fail:
@@ -255,11 +403,7 @@ window_fail:
 emwh_init_atom_replies_fail:
 atom_get_fail:
 fail:
-   xcb_ewmh_connection_wipe(&xwm.ewmh);
-
-   if (xwm.connection)
-      xcb_disconnect(xwm.connection);
-
+   wlc_xwm_deinit();
    return false;
 }
 
@@ -269,10 +413,18 @@ wlc_xwm_deinit()
    if (xwm.event_source)
       wl_event_source_remove(xwm.event_source);
 
-   xcb_ewmh_connection_wipe(&xwm.ewmh);
+   if (x11.window)
+      x11.api.xcb_destroy_window(x11.connection, x11.window);
 
-   if (xwm.connection)
-      xcb_disconnect(xwm.connection);
+   // inline in xcb_ewmh.h
+   xcb_ewmh_connection_wipe(&x11.ewmh);
+
+   if (x11.connection)
+      x11.api.xcb_disconnect(x11.connection);
+
+   if (x11.api.xcb_handle)
+      dlclose(x11.api.xcb_handle);
 
    memset(&xwm, 0, sizeof(xwm));
+   memset(&x11, 0, sizeof(x11));
 }
