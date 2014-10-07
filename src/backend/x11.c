2580fe2d88aef114bb05409f3f5e4e3e5e1db9a2
Jari Vetoniemi <mailroxas@gmail.com>
2014-10-08 01:32:48 +0300

    Whitespace

--- src/backend/x11.c
+++ src/backend/x11.c
@@ -457,14 +457,14 @@ wlc_x11_init(struct wlc_backend *out_backend, struct wlc_compositor *compositor)
    if ((error = x11.api.xcb_request_check(x11.connection, x11.api.xcb_change_window_attributes_checked(x11.connection, x11.screen->root, XCB_CW_EVENT_MASK, &root_mask)))) {
       uint32_t mask = XCB_CW_EVENT_MASK; // | XCB_CW_CURSOR;
       uint32_t values[] = {
-            XCB_EVENT_MASK_FOCUS_CHANGE |
-            XCB_EVENT_MASK_EXPOSURE |
-            XCB_EVENT_MASK_STRUCTURE_NOTIFY |
-            XCB_EVENT_MASK_POINTER_MOTION |
-            XCB_EVENT_MASK_BUTTON_PRESS |
-            XCB_EVENT_MASK_BUTTON_RELEASE |
-            XCB_EVENT_MASK_KEY_PRESS |
-            XCB_EVENT_MASK_KEY_RELEASE,
+         XCB_EVENT_MASK_FOCUS_CHANGE |
+         XCB_EVENT_MASK_EXPOSURE |
+         XCB_EVENT_MASK_STRUCTURE_NOTIFY |
+         XCB_EVENT_MASK_POINTER_MOTION |
+         XCB_EVENT_MASK_BUTTON_PRESS |
+         XCB_EVENT_MASK_BUTTON_RELEASE |
+         XCB_EVENT_MASK_KEY_PRESS |
+         XCB_EVENT_MASK_KEY_RELEASE,
          x11.cursor,
       };
 
