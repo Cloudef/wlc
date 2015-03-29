9aad475d4e29e5f8b5a45618f7c7795838bec314
Jari Vetoniemi <mailroxas@gmail.com>
2015-03-29 14:20:04 +0300

    x11.c: fix root wm detection

--- src/platform/backend/x11.c
+++ src/platform/backend/x11.c
@@ -536,19 +536,24 @@ update_outputs(struct chck_pool *outputs)
    uint32_t mask = XCB_CW_EVENT_MASK | XCB_CW_CURSOR;
    uint32_t values[] = {
       XCB_EVENT_MASK_FOCUS_CHANGE |
-         XCB_EVENT_MASK_EXPOSURE |
-         XCB_EVENT_MASK_STRUCTURE_NOTIFY |
-         XCB_EVENT_MASK_POINTER_MOTION |
-         XCB_EVENT_MASK_BUTTON_PRESS |
-         XCB_EVENT_MASK_BUTTON_RELEASE |
-         XCB_EVENT_MASK_KEY_PRESS |
-         XCB_EVENT_MASK_KEY_RELEASE,
+      XCB_EVENT_MASK_EXPOSURE |
+      XCB_EVENT_MASK_STRUCTURE_NOTIFY |
+      XCB_EVENT_MASK_POINTER_MOTION |
+      XCB_EVENT_MASK_BUTTON_PRESS |
+      XCB_EVENT_MASK_BUTTON_RELEASE |
+      XCB_EVENT_MASK_KEY_PRESS |
+      XCB_EVENT_MASK_KEY_RELEASE,
+      x11.cursor,
+   };
+
+   uint32_t root_values[] = {
+      values[0] | XCB_EVENT_MASK_SUBSTRUCTURE_REDIRECT | XCB_EVENT_MASK_SUBSTRUCTURE_NOTIFY,
       x11.cursor,
    };
 
    uint32_t count = 0;
    xcb_generic_error_t *error;
-   if ((error = x11.api.xcb_request_check(x11.connection, x11.api.xcb_change_window_attributes_checked(x11.connection, x11.screen->root, mask, values)))) {
+   if ((error = x11.api.xcb_request_check(x11.connection, x11.api.xcb_change_window_attributes_checked(x11.connection, x11.screen->root, mask, root_values)))) {
       free(error);
 
       for (uint32_t i = 0; i < fakes; ++i) {
