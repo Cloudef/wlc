f11ae2d7d18f75e400dcc3908228e1c59b7d2b8f
Jari Vetoniemi <mailroxas@gmail.com>
2015-03-31 21:07:39 +0300

    Move os compatibility functions to shared header

--- src/compositor/seat/keymap.c
+++ src/compositor/seat/keymap.c
@@ -1,9 +1,5 @@
-#include <stdlib.h>
-#include <unistd.h>
+#include "os-compatibility.h"
 #include <string.h>
-#include <sys/mman.h>
-#include <errno.h>
-#include <fcntl.h>
 #include <assert.h>
 #include <wlc/wlc.h>
 #include <wayland-server.h>
@@ -26,80 +22,6 @@ const char* WLC_LED_NAMES[WLC_LED_LAST] = {
    XKB_LED_NAME_CAPS,
    XKB_LED_NAME_SCROLL
 };
-
-static int
-set_cloexec_or_close(int fd)
-{
-   if (fd == -1)
-      return -1;
-
-   long flags;
-   if ((flags = fcntl(fd, F_GETFD)) == -1)
-      goto err;
-
-   if (fcntl(fd, F_SETFD, flags | FD_CLOEXEC) == -1)
-      goto err;
-
-   return fd;
-
-err:
-   close(fd);
-   return -1;
-}
-
-static int
-create_tmpfile_cloexec(char *tmpname)
-{
-   int fd;
-
-#ifdef HAVE_MKOSTEMP
-   if ((fd = mkostemp(tmpname, O_CLOEXEC)) >= 0)
-      unlink(tmpname);
-#else
-   if ((fd = mkstemp(tmpname)) >= 0) {
-      fd = set_cloexec_or_close(fd);
-      unlink(tmpname);
-   }
-#endif
-
-   return fd;
-}
-
-static int
-os_create_anonymous_file(off_t size)
-{
-   static const char template[] = "/loliwm-shared-XXXXXX";
-
-   const char *path = getenv("XDG_RUNTIME_DIR");
-   if (chck_cstr_is_empty(path))
-      return -1;
-
-   struct chck_string name = {0};
-   if (!chck_string_set_format(&name, "%s%s%s", path, (chck_cstr_ends_with(path, "/") ? "" : "/"), template))
-      return -1;
-
-   int fd = create_tmpfile_cloexec(name.data);
-   chck_string_release(&name);
-
-   if (fd < 0)
-      return -1;
-
-   int ret;
-#ifdef HAVE_POSIX_FALLOCATE
-   if ((ret = posix_fallocate(fd, 0, size)) != 0) {
-      close(fd);
-      errno = ret;
-      return -1;
-   }
-#else
-   if ((ret = ftruncate(fd, size)) < 0) {
-      close(fd);
-      return -1;
-   }
-#endif
-
-   return fd;
-}
 
 uint32_t
 wlc_keymap_get_mod_mask(struct wlc_keymap *keymap, uint32_t in)
