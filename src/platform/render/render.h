462b3d46cd41828154706f7fea601e5b81c055e4
Jari Vetoniemi <mailroxas@gmail.com>
2015-08-22 03:56:09 +0300

    render.h: Buffer may be null.

--- src/platform/render/render.h
+++ src/platform/render/render.h
@@ -19,7 +19,7 @@ struct wlc_render_api {
    WLC_NONULL void (*terminate)(struct ctx *render);
    WLC_NONULL void (*resolution)(struct ctx *render, const struct wlc_size *resolution);
    WLC_NONULL void (*surface_destroy)(struct ctx *render, struct wlc_context *bound, struct wlc_surface *surface);
-   WLC_NONULL bool (*surface_attach)(struct ctx *render, struct wlc_context *bound, struct wlc_surface *surface, struct wlc_buffer *buffer);
+   WLC_NONULLV(1,2,3) bool (*surface_attach)(struct ctx *render, struct wlc_context *bound, struct wlc_surface *surface, struct wlc_buffer *buffer);
    WLC_NONULL void (*view_paint)(struct ctx *render, struct wlc_view *view);
    WLC_NONULL void (*surface_paint)(struct ctx *render, struct wlc_surface *surface, struct wlc_origin *pos);
    WLC_NONULL void (*pointer_paint)(struct ctx *render, struct wlc_origin *pos);
@@ -36,7 +36,7 @@ struct wlc_render {
 
 WLC_NONULL void wlc_render_resolution(struct wlc_render *render, struct wlc_context *bound, const struct wlc_size *resolution);
 WLC_NONULL void wlc_render_surface_destroy(struct wlc_render *render, struct wlc_context *bound, struct wlc_surface *surface);
-WLC_NONULL bool wlc_render_surface_attach(struct wlc_render *render, struct wlc_context *bound, struct wlc_surface *surface, struct wlc_buffer *buffer);
+WLC_NONULLV(1,2,3) bool wlc_render_surface_attach(struct wlc_render *render, struct wlc_context *bound, struct wlc_surface *surface, struct wlc_buffer *buffer);
 WLC_NONULL void wlc_render_view_paint(struct wlc_render *render, struct wlc_context *bound, struct wlc_view *view);
 WLC_NONULL void wlc_render_surface_paint(struct wlc_render *render, struct wlc_context *bound, struct wlc_surface *surface, struct wlc_origin *pos);
 WLC_NONULL void wlc_render_pointer_paint(struct wlc_render *render, struct wlc_context *bound, struct wlc_origin *pos);
