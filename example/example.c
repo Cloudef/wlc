498a18172c709836ec297235d8e841f29fbf2af0
Jari Vetoniemi <mailroxas@gmail.com>
2015-08-29 23:23:42 +0300

    example.c: Better resize code.

--- example/example.c
+++ example/example.c
@@ -8,7 +8,6 @@ static struct {
    struct {
       wlc_handle view;
       struct wlc_origin grab;
-      struct wlc_geometry geometry;
       uint32_t edges;
    } action;
 } compositor;
@@ -16,13 +15,12 @@ static struct {
 static bool
 start_interactive_action(wlc_handle view, const struct wlc_origin *origin)
 {
-   const struct wlc_geometry *g;
-   if (compositor.action.view || !(g = wlc_view_get_geometry(view)))
+   if (compositor.action.view)
       return false;
 
    compositor.action.view = view;
    compositor.action.grab = *origin;
-   compositor.action.geometry = *g;
+   wlc_view_bring_to_front(view);
    return true;
 }
 
@@ -35,11 +33,12 @@ start_interactive_move(wlc_handle view, const struct wlc_origin *origin)
 static void
 start_interactive_resize(wlc_handle view, uint32_t edges, const struct wlc_origin *origin)
 {
-   if (!start_interactive_action(view, origin))
+   const struct wlc_geometry *g;
+   if (!(g = wlc_view_get_geometry(view)) || !start_interactive_action(view, origin))
       return;
 
-   const int32_t halfw = compositor.action.geometry.origin.x + compositor.action.geometry.size.w / 2;
-   const int32_t halfh = compositor.action.geometry.origin.y + compositor.action.geometry.size.h / 2;
+   const int32_t halfw = g->origin.x + g->size.w / 2;
+   const int32_t halfh = g->origin.y + g->size.h / 2;
 
    if (!(compositor.action.edges = edges)) {
       compositor.action.edges = (origin->x < halfw ? WLC_RESIZE_EDGE_LEFT : (origin->x > halfw ? WLC_RESIZE_EDGE_RIGHT : 0)) |
@@ -185,42 +184,43 @@ pointer_motion(wlc_handle handle, uint32_t time, const struct wlc_origin *origin
    (void)handle, (void)time;
 
    if (compositor.action.view) {
-      struct wlc_geometry *g = &compositor.action.geometry;
+      const int32_t dx = origin->x - compositor.action.grab.x;
+      const int32_t dy = origin->y - compositor.action.grab.y;
+      struct wlc_geometry g = *wlc_view_get_geometry(compositor.action.view);
 
       if (compositor.action.edges) {
          const struct wlc_size min = { 80, 40 };
-         const int32_t wd = chck_max32(0, origin->x - g->origin.x);
-         const int32_t hd = chck_max32(0, origin->y - g->origin.y);
 
+         struct wlc_geometry n = g;
          if (compositor.action.edges & WLC_RESIZE_EDGE_LEFT) {
-            const uint32_t tw = chck_max32(0, g->size.w - (origin->x - g->origin.x));
-            g->size.w = chck_maxu32(min.w, tw);
-            g->origin.x = (g->size.w > min.w ? origin->x : g->origin.x);
+            n.size.w -= dx;
+            n.origin.x += dx;
          } else if (compositor.action.edges & WLC_RESIZE_EDGE_RIGHT) {
-            g->size.w = chck_maxu32(min.w, wd);
+            n.size.w += dx;
          }
 
          if (compositor.action.edges & WLC_RESIZE_EDGE_TOP) {
-            const uint32_t th = chck_max32(0, g->size.h - (origin->y - g->origin.y));
-            g->size.h = chck_maxu32(min.h, th);
-            g->origin.y = (g->size.h > min.h ? origin->y : g->origin.y);
+            n.size.h -= dy;
+            n.origin.y += dy;
          } else if (compositor.action.edges & WLC_RESIZE_EDGE_BOTTOM) {
-            g->size.h = chck_maxu32(min.h, hd);
+            n.size.h += dy;
          }
 
-         const struct wlc_geometry *c;
-         if ((c = wlc_view_get_geometry(compositor.action.view))) {
-            const struct wlc_geometry r = { g->origin, g->size };
-            wlc_view_set_geometry(compositor.action.view, compositor.action.edges, &r);
+         if (n.size.w >= min.w) {
+            g.origin.x = n.origin.x;
+            g.size.w = n.size.w;
          }
+
+         if (n.size.h >= min.h) {
+            g.origin.y = n.origin.y;
+            g.size.h = n.size.h;
+         }
+
+         wlc_view_set_geometry(compositor.action.view, compositor.action.edges, &g);
       } else {
-         const int32_t dx = origin->x - compositor.action.grab.x;
-         const int32_t dy = origin->y - compositor.action.grab.y;
-
-         g->origin.x += dx;
-         g->origin.y += dy;
-
-         wlc_view_set_geometry(compositor.action.view, 0, g);
+         g.origin.x += dx;
+         g.origin.y += dy;
+         wlc_view_set_geometry(compositor.action.view, 0, &g);
       }
 
       compositor.action.grab = *origin;
