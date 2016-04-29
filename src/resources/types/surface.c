35caed59fa992a82774711622d86ac1172abd07b
Jari Vetoniemi <mailroxas@gmail.com>
2016-04-29 22:28:12 +0300

    surface: Avoid potential division by zero

--- src/resources/types/surface.c
+++ src/resources/types/surface.c
@@ -293,9 +293,11 @@ wlc_surface_attach_to_output(struct wlc_surface *surface, struct wlc_output *out
       struct wlc_view *view = convert_from_wlc_handle(surface->view, "view");
       struct wlc_geometry g, area;
 
+      struct wlc_size ssize = { 1, 1 };
+      wlc_size_max(&ssize, &surface->size, &ssize);
       wlc_view_get_bounds(view, &g, &area);
-      surface->coordinate_transform.w = (float)(area.size.w) / surface->size.w;
-      surface->coordinate_transform.h = (float)(area.size.h) / surface->size.h;
+      surface->coordinate_transform.w = (float)(area.size.w) / ssize.w;
+      surface->coordinate_transform.h = (float)(area.size.h) / ssize.h;
    }
 
    surface->commit.attached = (buffer ? true : false);
