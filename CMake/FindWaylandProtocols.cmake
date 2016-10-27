f8bb91b55cf70a59833996d95b95c27f323df39c
Jari Vetoniemi <mailroxas@gmail.com>
2016-10-27 14:38:48 +0300

    CMake: s/WLPROTO/WAYLANDPROTOCOLS/
    The variables has to be same as the package name in filename, looks ugly
    but will mess up with some cmake "features" otherwise.

--- CMake/FindWaylandProtocols.cmake
+++ CMake/FindWaylandProtocols.cmake
@@ -1,5 +1,5 @@
 #.rst:
-# Findwlproto
+# FindWaylandProtocols
 # -------
 #
 # Find wayland protocol description files
@@ -8,8 +8,8 @@
 #
 # ::
 #
-#   WLPROTO_FOUND         - True if wayland protocol files are available
-#   WLPROTO_PATH          - Path to wayland protocol files
+#   WAYLANDPROTOCOLS_FOUND         - True if wayland protocol files are available
+#   WAYLANDPROTOCOLS_PATH          - Path to wayland protocol files
 #
 #=============================================================================
 # Copyright (c) 2015 Jari Vetoniemi
@@ -22,18 +22,18 @@
 #=============================================================================
 
 include(FeatureSummary)
-set_package_properties(wlproto PROPERTIES
+set_package_properties(WaylandProtocols PROPERTIES
    URL "https://cgit.freedesktop.org/wayland/wayland-protocols"
    DESCRIPTION "Wayland protocol development")
 
-unset(WLPROTO_PATH)
+unset(WAYLANDPROTOCOLS_PATH)
 
 find_package(PkgConfig)
 if (PKG_CONFIG_FOUND)
    execute_process(COMMAND ${PKG_CONFIG_EXECUTABLE} --variable=pkgdatadir wayland-protocols
-      OUTPUT_VARIABLE WLPROTO_PATH OUTPUT_STRIP_TRAILING_WHITESPACE)
+       OUTPUT_VARIABLE WAYLANDPROTOCOLS_PATH OUTPUT_STRIP_TRAILING_WHITESPACE)
 endif ()
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(WaylandProtocols DEFAULT_MSG WLPROTO_PATH WLPROTO_PATH)
-mark_as_advanced(WLPROTO_PATH)
+find_package_handle_standard_args(WaylandProtocols DEFAULT_MSG WAYLANDPROTOCOLS_PATH)
+mark_as_advanced(WAYLANDPROTOCOLS_PATH)
