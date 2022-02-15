e943a54d6b484fb1037702dbe667913ab7946c68
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-16 00:05:41 +0800

    Delete CMake directory

--- CMake/test.cmake
+++
@@ -1,8 +0,0 @@
-# Wraps add_test
-# Fixes win32 .exe extensions on mingw at least
-# Uses CTEST_EXEC_WITH variable which you can set to for example valgrind to run tests with valgrind.
-# Also uses CTEST_OUTPUT_DIRECTORY to set global output directory
-
-function(add_test_ex target)
-   add_test(${target} ${CTEST_EXEC_WITH} "${CTEST_OUTPUT_DIRECTORY}/${target}${CMAKE_EXECUTABLE_SUFFIX}")
-endfunction()
