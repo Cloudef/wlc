58015c371e9e0439221a3f5e4e6aafe8e4bd2595
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-16 00:11:06 +0800

    Delete src directory

--- src/platform/render/gles2.h
+++
@@ -1,8 +0,0 @@
-#ifndef _WLC_GLES2_H_
-#define _WLC_GLES2_H_
-
-struct wlc_render_api;
-
-void* wlc_gles2(struct wlc_render_api *api);
-
-#endif /* _WLC_GLES2_H_ */
--- src/resources/types/xdg-toplevel.h
+++
@@ -1,9 +0,0 @@
-#ifndef _WLC_XDG_TOPLEVEL_H_
-#define _WLC_XDG_TOPLEVEL_H_
-
-#include <wayland-server.h>
-#include "wayland-xdg-shell-unstable-v6-server-protocol.h"
-
-const struct zxdg_toplevel_v6_interface* wlc_xdg_toplevel_implementation(void);
-
-#endif /* _WLC_XDG_TOPLEVEL_H_ */
