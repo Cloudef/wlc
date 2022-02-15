cf4f388458b990f175317a01d22c91f7e748dbee
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-16 00:11:44 +0800

    Delete src directory

--- src/resources/types/shell-surface.h
+++
@@ -1,8 +0,0 @@
-#ifndef _WLC_SHELL_SURFACE_H_
-#define _WLC_SHELL_SURFACE_H_
-
-#include <wayland-server-protocol.h>
-
-const struct wl_shell_surface_interface* wlc_shell_surface_implementation(void);
-
-#endif /* _WLC_SHELL_SURFACE_H_ */
